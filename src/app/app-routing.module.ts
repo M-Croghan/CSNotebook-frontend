3a3c882544244388cd9f4d116d43a9a8e960611e
Michael Croghan <mcrog.7@gmail.com>
2022-01-30 12:56:16 -0500

    Set-up Login Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
+import { ExploreComponent } from './explore/explore.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { StudyComponent } from './study/study.component';
 
 const routes: Routes = [
   {
@@ -12,6 +15,18 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'explore',
+    component: ExploreComponent
+  },
+  {
+    path: 'study',
+    component: StudyComponent
   }
 ];
 
