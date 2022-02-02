ef8ea4dec917b6bd0e3ed6d6ee4fd2b82478ece9
Michael Croghan <mcrog.7@gmail.com>
2022-02-02 11:53:05 -0500

    Provide appropriate url endpoints for register / login to the auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CONNECTION } from 'src/connection';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  api = CONNECTION;
+  private _registerUrl = `${this.api}/auth/users/register`
+  private _loginUrl = `${this.api}/auth/users/login`
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  registerUser(user){
+    return this.http.post<any>(this._registerUrl, user)
+  }
+
 }
