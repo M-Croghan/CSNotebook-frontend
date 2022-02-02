1f77421cd759ecc9fc1b313f9e85373598965dec
Michael Croghan <mcrog.7@gmail.com>
2022-02-01 20:29:21 -0500

    Complete Login form funtionality to swap between login & registering a new user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email: string = '';
+  password: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+
+  login(){
+    console.log('LOGIN');
+  }
+
+  register(){
+    console.log('REGISTER');
+  }
+
+
+  swapLoginRegister(){
+    let button = document.querySelector('#execute')
+    let heading = document.querySelector('#login-lbl')
+    let swapLabel = document.querySelector('#switch')
+
+    if (heading?.textContent === 'LOGIN' && swapLabel?.textContent != null && button?.textContent != null){
+      heading.textContent = 'REGISTER';
+      swapLabel.textContent = "Already a User? Click Here."
+      button.textContent = "REGISTER";
+    }
+    else if (heading?.textContent === 'REGISTER' && swapLabel?.textContent != null && button?.textContent != null){
+      heading.textContent = 'LOGIN';
+      swapLabel.textContent = "New to CS Notebook? Register Here."
+      button.textContent = "LOGIN";
+    }
   }
 
 }
